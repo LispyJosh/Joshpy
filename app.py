e463266eabd6b72b5cdbf51c88bada73a1df5dbe
LispyJosh <jcroly1998@gmail.com>
2024-09-13 11:51:12 +0100

    added SQLAlchemy model + CRUD to app.py

--- app.py
+++ app.py
@@ -59,7 +59,47 @@ def timer_end():
 def todo():
     return render_template('to-do.html')
 
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/todo_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Model for To-Do items
+class TodoItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<Task {self.id}: {self.task}>'
+
+# Create the database
+with app.app_context():
+    db.create_all()
 
 
+from flask import request, jsonify
 
+@app.route('/add-todo', methods=['POST'])
+def add_todo():
+    task_data = request.json
+    new_task = TodoItem(task=task_data['task'])
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({"status": "success", "task": new_task.task})
+
+@app.route('/get-todos', methods=['GET'])
+def get_todos():
+    todos = TodoItem.query.all()
+    return jsonify([{"id": todo.id, "task": todo.task} for todo in todos])
+
+@app.route('/delete-todo/<int:id>', methods=['DELETE'])
+def delete_todo(id):
+    todo = TodoItem.query.get(id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+        return jsonify({"status": "success", "message": "Task deleted"})
+    return jsonify({"status": "error", "message": "Task not found"}), 404
 
